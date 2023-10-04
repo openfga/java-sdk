08392cb50c4544865f4b0be0d3aa2e8b697d99eb
Justin "J.R." Hill <justin@so.dang.cool>
2023-10-04 10:06:32 -0700

    refactor: refactor http request attempts

--- src/main/java/dev/openfga/sdk/api/client/HttpRequestAttempt.java
+++ src/main/java/dev/openfga/sdk/api/client/HttpRequestAttempt.java
@@ -34,47 +34,41 @@ public class HttpRequestAttempt<T> {
 
     public CompletableFuture<T> attemptHttpRequest() throws ApiException {
         int retryNumber = 0;
-        return attemptHttpRequest(apiClient.getHttpClient(), retryNumber);
+        return attemptHttpRequest(apiClient.getHttpClient(), retryNumber, null);
     }
 
-    private CompletableFuture<T> attemptHttpRequest(HttpClient httpClient, int retryNumber) {
+    private CompletableFuture<T> attemptHttpRequest(HttpClient httpClient, int retryNumber, Throwable previousError) {
         return httpClient
                 .sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenCompose(response -> {
-                    int status = response.statusCode();
-                    String responseBody = response.body();
-
                     try {
-                        checkStatus(name, response);
-                    } catch (FgaApiRateLimitExceededError | FgaApiInternalError e) {
+                        checkStatus(name, response, previousError);
+                    } catch (FgaApiRateLimitExceededError | FgaApiInternalError retryableError) {
                         if (retryNumber < configuration.getMaxRetries()) {
                             HttpClient delayingClient = getDelayedHttpClient();
-                            return attemptHttpRequest(delayingClient, retryNumber + 1);
+                            return attemptHttpRequest(delayingClient, retryNumber + 1, retryableError);
                         }
-                        return CompletableFuture.failedFuture(e);
+                        return CompletableFuture.failedFuture(retryableError);
                     } catch (ApiException e) {
                         return CompletableFuture.failedFuture(e);
                     }
 
-                    if (status != HttpURLConnection.HTTP_OK
-                            && status != HttpURLConnection.HTTP_CREATED
-                            && status != HttpURLConnection.HTTP_NO_CONTENT) {
-                        // An HTTP failure that is not modeled in checkStatus(..., ...) below.
-                        return CompletableFuture.failedFuture(new ApiException(name, response));
-                    }
-
-                    if (clazz == Void.class && isNullOrWhitespace(responseBody)) {
-                        return CompletableFuture.completedFuture(null);
-                    }
-
-                    try {
-                        T body = apiClient.getObjectMapper().readValue(responseBody, clazz);
-                        return CompletableFuture.completedFuture(body);
-                    } catch (IOException e) {
-                        // Malformed response.
-                        return CompletableFuture.failedFuture(new ApiException(e));
-                    }
+                    return deserializeResponse(response);
                 });
+    }
+
+    private CompletableFuture<T> deserializeResponse(HttpResponse<String> response) {
+        if (clazz == Void.class && isNullOrWhitespace(response.body())) {
+            return CompletableFuture.completedFuture(null);
+        }
+
+        try {
+            T deserialized = apiClient.getObjectMapper().readValue(response.body(), clazz);
+            return CompletableFuture.completedFuture(deserialized);
+        } catch (IOException e) {
+            // Malformed response.
+            return CompletableFuture.failedFuture(new ApiException(e));
+        }
     }
 
     private HttpClient getDelayedHttpClient() {
@@ -85,30 +79,28 @@ public class HttpRequestAttempt<T> {
                 .build();
     }
 
-    private static void checkStatus(String name, HttpResponse<String> response)
-            throws FgaApiValidationError, FgaApiAuthenticationError, FgaApiNotFoundError, FgaApiRateLimitExceededError,
-                    FgaApiInternalError {
-
+    private static void checkStatus(String name, HttpResponse<String> response, Throwable previousError)
+            throws ApiException {
         int status = response.statusCode();
         String body = response.body();
 
         switch (status) {
             case HttpURLConnection.HTTP_BAD_REQUEST:
             case 422: // HTTP 422 Unprocessable Entity
-                throw new FgaApiValidationError(name, status, response.headers(), body);
+                throw new FgaApiValidationError(name, previousError, status, response.headers(), body);
 
             case HttpURLConnection.HTTP_UNAUTHORIZED:
             case HttpURLConnection.HTTP_FORBIDDEN:
-                throw new FgaApiAuthenticationError(name, status, response.headers(), body);
+                throw new FgaApiAuthenticationError(name, previousError, status, response.headers(), body);
 
             case HttpURLConnection.HTTP_NOT_FOUND:
-                throw new FgaApiNotFoundError(name, status, response.headers(), body);
+                throw new FgaApiNotFoundError(name, previousError, status, response.headers(), body);
 
             case 429: // HTTP 429 Too Many Requests
-                throw new FgaApiRateLimitExceededError(name, status, response.headers(), body);
+                throw new FgaApiRateLimitExceededError(name, previousError, status, response.headers(), body);
 
             case HttpURLConnection.HTTP_INTERNAL_ERROR:
-                throw new FgaApiInternalError(name, status, response.headers(), body);
+                throw new FgaApiInternalError(name, previousError, status, response.headers(), body);
         }
     }
 }
