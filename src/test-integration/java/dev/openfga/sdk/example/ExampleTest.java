fa980300614b6cd2e17a19c0444bc50096c253ba
J.R. Hill <justin@so.dang.cool>
2023-12-27 14:42:03 -0800

    refactor: simplify auth model loading for integ test

--- src/test-integration/java/dev/openfga/sdk/example/ExampleTest.java
+++ src/test-integration/java/dev/openfga/sdk/example/ExampleTest.java
@@ -3,8 +3,11 @@ package dev.openfga.sdk.example;
 import org.junit.jupiter.api.Test;
 
 public class ExampleTest {
+    private final Example1 example1 = new Example1();
+
     @Test
-    public void test() {
-        Example1.main(null);
+    public void example1() throws Exception {
+        example1.module = "test-integration";
+        example1.run();
     }
 }
