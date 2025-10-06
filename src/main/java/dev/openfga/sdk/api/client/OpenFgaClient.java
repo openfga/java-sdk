705472f56239ff8ab0ce8d2c7aa4342612c30c42
Ewan Harris <ewan.harris@okta.com>
2025-10-06 11:22:05 +0100

    fix(readAuthorizationModel): correctly handle options with no modelID set

--- src/main/java/dev/openfga/sdk/api/client/OpenFgaClient.java
+++ src/main/java/dev/openfga/sdk/api/client/OpenFgaClient.java
@@ -248,7 +248,11 @@ public class OpenFgaClient {
             ClientReadAuthorizationModelOptions options) throws FgaInvalidParameterException {
         configuration.assertValid();
         String storeId = configuration.getStoreIdChecked();
-        String authorizationModelId = options.getAuthorizationModelIdChecked();
+        // Set authorizationModelId from options if available; otherwise, use the default from configuration
+        String authorizationModelId = !isNullOrWhitespace(options.getAuthorizationModelId())
+                ? options.getAuthorizationModelIdChecked()
+                : configuration.getAuthorizationModelId();
+
         var overrides = new ConfigurationOverride().addHeaders(options);
         return call(() -> api.readAuthorizationModel(storeId, authorizationModelId, overrides))
                 .thenApply(ClientReadAuthorizationModelResponse::new);
